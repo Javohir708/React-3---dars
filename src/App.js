d73e3b31d11a2fb414c8ea35519ab4dd02c38b76
Javohir708 <joxauzking@gamil.com>
2025-01-11 19:56:20 +0500

    social qismi tayyor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import iphonePro from './assets/iphone.svg'
 import watch from './assets/WATCH.svg'
 import watchfitnes from './assets/social.svg'
 import ipad from './assets/ipad.svg'
+import mac from './assets/mac.svg'
+import smartWatch from './assets/SWatch.svg'
 
 function App() {
   return (
@@ -16,8 +18,12 @@ function App() {
       <Hero title="iPhone 16 Pro" info="Hello, Apple Intelligence." url={iphone16Pro} btn={"Learn more"} buy={<div style={{ color: "dodgerblue", }}>Buy</div>}/>
       <Hero title="iPhone 16" info="Hello, Apple Intelligence." url={iphonePro} btn={"Learn more"} buy={<div style={{ color: "#fff", }}>Buy</div>}/>
       <Hero title={<div style={{ color: "#000" }}>WATCH</div>}  info={<div style={{ color: "#000" }}>SERIES 10</div>}  url={watch} btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>Buy</div>}/>
-      <Socialinfo title="Fitness+" info="Fitness for everyone. Personalized for you." fitnes={watchfitnes} btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>Try it free</div>}/>
-      <Socialinfo title="Fitness+" info="Fitness for everyone. Personalized for you." fitnes={watchfitnes} btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>Try it free</div>}/>
+      <div className='social-wrapper'>
+      <Socialinfo title="Fitness+" info="Fitness for everyone. Personalized for you." url={watchfitnes}  btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>Try it free</div>}/>
+      <Socialinfo title="iPad air" info="Two sizes. Faster chip. Does it all." url={ipad}  btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>buy</div>}/>
+      <Socialinfo title={<div style={{ color: "#fff" }}>MacBook Pro</div>}  info={<div style={{ color: "#fff" }}>A work of smart.</div>} url={mac}  btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>buy</div>}/>
+      <Socialinfo title={<div style={{ color: "#fff" }}>Watch</div>} about={<div style={{ color: "orange" }}>ULTRA 2</div>} info={<div style={{ color: "#fff" }}>A work of smart.</div>} url={smartWatch}  btn={"Learn more"} buy={<div style={{ color: "dodgerblue" }}>buy</div>}/>
+      </div>
     </div>
   );
 }
--- src/components/socialinfo/Socialinfo.js
+++ src/components/socialinfo/Socialinfo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Socialinfo.scss"
 
-const Socialinfo = ({title, info, fitnes, btn, buy}) => {
+const Socialinfo = ({title, about, info, url, btn, buy}) => {
   return (
     <section className='social'>
             <div className='social__content-wrapper'>
-            <div style={{background: `url(${fitnes}) no-repeat center/cover`}} className='social__content'>
+            <div style={{background: `url(${url}) no-repeat center/cover`}} className='social__content'>
                 <h2>{title}</h2>
+                <h4>{about}</h4>
                 <p>{info}</p>
                 <div className='global-btn'>
                     <button className='global-learn-btn'>{btn}</button>
